e3dbf535089e4ccdb2285bfaaff94efde490b5a2
GabotThomas <gabotthomas@gmail.com>
2024-08-02 16:31:35 +0200

    test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import express from 'express';
-import sharp from 'sharp';
+// import express from 'express';
+// import sharp from 'sharp';
+const express = require('express');
 
 const app = express();
 
@@ -15,62 +16,62 @@ app.get('/image', (req, res) => {
 	res.send('<h1>Hello from SHARP IMG</h1>');
 });
 
-app.post('/image/sharp', express.json({ limit: '20mb' }), async (req, res) => {
-    if (!req.body) {
-        return res.status(400).send('No image are sended.');
-    }
+// app.post('/image/sharp', express.json({ limit: '20mb' }), async (req, res) => {
+//     if (!req.body) {
+//         return res.status(400).send('No image are sended.');
+//     }
   
-    try { 
-        // convert array buffer to buffer
-        const imageBuffer = Buffer.from(req.body.image.data);
+//     try { 
+//         // convert array buffer to buffer
+//         const imageBuffer = Buffer.from(req.body.image.data);
 
 
-        // Traiter l'image avec sharp
-        let processedImage = null;
-        try{
-          processedImage = sharp(imageBuffer);
-        }catch(error){
-            console.log('error', error.slice(0, 100) ,"...", error.slice(-100));
-          return res.status(500).send('Error processing the image.');
-        }
+//         // Traiter l'image avec sharp
+//         let processedImage = null;
+//         try{
+//           processedImage = sharp(imageBuffer);
+//         }catch(error){
+//             console.log('error', error.slice(0, 100) ,"...", error.slice(-100));
+//           return res.status(500).send('Error processing the image.');
+//         }
         
-        // get the metadata of the image
-        const metadata = await processedImage.metadata();
+//         // get the metadata of the image
+//         const metadata = await processedImage.metadata();
         
-        res.send(JSON.stringify(metadata));
-    } catch (error) {
-      res.status(500).send('Error processing the image.');
-    }
-  });
+//         res.send(JSON.stringify(metadata));
+//     } catch (error) {
+//       res.status(500).send('Error processing the image.');
+//     }
+//   });
 
-app.post('/image/sharp/:size', express.json({  limit: '20mb' }), async (req, res) => {
+// app.post('/image/sharp/:size', express.json({  limit: '20mb' }), async (req, res) => {
 
-    const size = parseInt(req.params.size);
+//     const size = parseInt(req.params.size);
     
-    if (!req.body || isNaN(size)) {
-        return res.status(400).send('No image or invalid size.');
-    }
+//     if (!req.body || isNaN(size)) {
+//         return res.status(400).send('No image or invalid size.');
+//     }
   
-    try {
-      const imageBuffer = Buffer.from(req.body.image.data);
-      let processedImage = null;
-      try{
-         processedImage = await sharp(imageBuffer)
-            .resize(size) // Exemple de redimensionnement
-            .toBuffer();
-      }catch(error){
-            console.error(error);
-            // console.log('error', error.slice(0, 100) ,"...", error.slice(-100));
-            return res.status(500).send('Error processing the image.');
-      }
+//     try {
+//       const imageBuffer = Buffer.from(req.body.image.data);
+//       let processedImage = null;
+//       try{
+//          processedImage = await sharp(imageBuffer)
+//             .resize(size) // Exemple de redimensionnement
+//             .toBuffer();
+//       }catch(error){
+//             console.error(error);
+//             // console.log('error', error.slice(0, 100) ,"...", error.slice(-100));
+//             return res.status(500).send('Error processing the image.');
+//       }
 
 
-        res.send(JSON.stringify({image: processedImage}));
+//         res.send(JSON.stringify({image: processedImage}));
 
-    } catch (error) {
-        console.log("err",error);
-        res.status(500).send('Error processing the image.');
-    }
-  });
+//     } catch (error) {
+//         console.log("err",error);
+//         res.status(500).send('Error processing the image.');
+//     }
+//   });
 
 app.listen(3000);
